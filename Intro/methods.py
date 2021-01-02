5ee65ae49116397dba3aa56877b89a8494816bc2
CES <carloernestosarli@gmail.com>
2021-01-02 13:50:41 +0000

    added method def

--- Intro/methods.py
+++ Intro/methods.py
@@ -1,1 +1,12 @@
 # methods definitions
+# methods or functions are blocks of code that are only executed when called
+# all methods are defined using snake_case
+# methods can take arguments (or parameters) that we "pass in" separated by a comma
+# all methods are declared using the def keyword
+
+def say_hello(name: str):
+    print('hello ' + name + '!')
+
+
+say_hello('Natalia')
+say_hello('Carlo')
