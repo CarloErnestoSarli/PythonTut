effde72cc2a39c74f1631202a9552435e74b318c
taliaora <ginipuccini@gmail.com>
2021-01-13 19:05:43 +0000

    Here, my homework, lova ya meastro

--- Intro/userInput.py
+++ Intro/userInput.py
@@ -4,26 +4,69 @@ import sys
 print('What is your name?')
  
 # Stores everything typed up until ENTER
-name = sys.stdin.readline()
+# name = sys.stdin.readline()
  
-print('Hello', name)
+# print('Hello', name)
 
 
 # game time 
 
-def add_two(a: int, b: int):
-    return a+b
+
+def add_four(a: int, b: int, c: int, d: int):
+    return a+b+c+d
+
+def dividing(a:int, b:int):
+    return b/a
 
 while True:
-    print('first number please')
-    a: int = int(sys.stdin.readline())
-    print('second number please')
-    b: int = int(sys.stdin.readline())
-    result: int = add_two(a, b)
+    print('first number please') 
+    inputHappy = False
+    while inputHappy is not True:
+        input = sys.stdin.readline().rstrip()
+        if input.isdigit():
+
+            a: int = int(input) #casting
+            inputHappy = True
+        else:
+            print("give me a digit fucker")
+            
+    print('second number please')    
+    inputHappy = False
+    while inputHappy is not True:
+        input = sys.stdin.readline().rstrip()
+        if input.isdigit():
+
+            b: int = int(input) #casting
+            inputHappy = True
+        else:
+            print("give me a digit fucker")
+
+    print('third number please') 
+    inputHappy = False       
+    while inputHappy is False:
+        input = sys.stdin.readline().rstrip()
+        if input.isdigit():
+
+            c: int = int(input) #casting
+            inputHappy = True
+        else:
+            print("give me a digit fucker")
+    print('four number please')  
+    inputHappy = False      
+    while inputHappy is False:
+        input = sys.stdin.readline().rstrip()
+        if input.isdigit():
+
+            d: int = int(input) #casting
+            inputHappy = True
+        else:
+            print("give me a digit fucker")
+
+    result: int = add_four(a, b, c, d)
     print('result: '  f'{result}')
     print('press q to quit or any other key to continue')
-    c: str = sys.stdin.readline().rstrip()
-    if c == 'q':
+    e: str = sys.stdin.readline().rstrip()
+    if e == 'q':
         break
     else: 
         continue
@@ -36,4 +79,5 @@ while True:
 # what if the user would like to input 3 or 4 numbers?
 # can you do that?
 # can you extend our little calculator to do divisions too?
+# can you split the duplicate code into nice functions
 # have fun
